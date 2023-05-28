7746b132f74534e5170a842efd5ab5a0516078f5
zwj <mailzwj@126.com>
2023-05-28 22:06:15 +0800

    feat: 新增两个主题

--- src/constants.ts
+++ src/constants.ts
@@ -16,5 +16,27 @@ export const themes: Record<string, IClock.ITheme> = {
     minutePointerColor: 'rgba(0, 0, 0, 0.85)',
     milliPointerColor: 'rgba(242, 162, 60, 1)',
     centerCircleColor: '#fff'
+  },
+  'apple-light': {
+    backgroundColor: '#fff',
+    faceColor: 'rgba(0, 0, 0, 1)',
+    hourScaleColor: 'rgba(255, 255, 255, 0.6)',
+    minuteScaleColor: 'rgba(255, 255, 255, 0.3)',
+    textColor: 'rgba(255, 255, 255, 0.6)',
+    hourPointerColor: 'rgba(255, 255, 255, 0.85)',
+    minutePointerColor: 'rgba(255, 255, 255, 0.85)',
+    milliPointerColor: 'rgba(242, 162, 60, 1)',
+    centerCircleColor: '#fff'
+  },
+  'mi': {
+    backgroundColor: 'rgba(238, 113, 46, 1)',
+    faceColor: '#fff',
+    hourScaleColor: 'rgba(238, 113, 46, 0.6)',
+    minuteScaleColor: 'rgba(238, 113, 46, 0.3)',
+    textColor: 'rgba(238, 113, 46, 1)',
+    hourPointerColor: 'rgba(238, 113, 46, 1)',
+    minutePointerColor: 'rgba(238, 113, 46, 1)',
+    milliPointerColor: 'rgba(242, 162, 60, 1)',
+    centerCircleColor: '#fff'
   }
 };
